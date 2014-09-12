3fd968658752c44d428ef42a8b5ac074278a3dc6
Steve Lacy <me@slacy.me>
2014-09-12 15:31:29 -0700

    normalize

--- rest-todo/client/models/Todo.js
+++ rest-todo/client/models/Todo.js
@@ -1,10 +1,9 @@
 var  fission = require('../app');
 
 module.exports = fission.model({
-  idAttribute: '_id',
   url: '/v1/todos',
   props: {
-    _id: 'string',
+    id: 'string',
     text: 'string',
     done: 'boolean'
   },
